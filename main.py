51fa536e25a0c54573d24a63f9841dd1c1d1068c
21123-school <21123@haurakiplains.school.nz>
2025-05-27 10:47:25 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+import sys
+import json
+
+# pygame set up
+pygame.init()
+screen = pygame.display.set_mode((18*45, 14*45))
+clock = pygame.time.Clock()
+fps = 120
+
+
+sans = pygame.font.SysFont('Comic Sans MS', 50)
+starttxt = sans.render('Dungeon Run', False, (0, 0, 0))
+sans = pygame.font.SysFont('Comic Sans MS', 20)
+start = sans.render('Press ENTER to start', False, (0, 0, 0))
+
+musicsound = pygame.mixer.Sound("sound/music.wav")
+
+def loadjson(f):
+    with open(f, 'rb') as file:
+        o = json.load(file)
+    return o
+
+musicsound.play(-1)
+
+while True:
+    dt = clock.tick(fps) / 1000
+    keys = pygame.key.get_pressed()
+
+    screen.fill((34, 34, 34))
+
+    screen.blit(sans.render('score: ' + 'beans', False, (255, 255, 255)), (15, 0, 50, 50))
+
+    pygame.display.update()
+    clock.tick(fps)
