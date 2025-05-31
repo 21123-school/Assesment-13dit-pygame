875b109c90cf13fe6e56c4438a21058850362a11
21123-school <21123@haurakiplains.school.nz>
2025-05-31 17:07:20 +1200

    change paktool

---
+++ paktool.py
@@ -0,0 +1,23 @@
+import os
+import zipfile
+    
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), 
+                       os.path.relpath(os.path.join(root, file), 
+                                       os.path.join(path, '..')))
+
+name = input('name folder for compression: ')
+
+if input('do you really what to build data.pak [Y/N]: ').lower() == 'y':
+    try:
+        os.remove(f"{name}.pak")
+        print('old file removed making new file..')
+    except:
+        print('not found making new file..')
+    with zipfile.ZipFile(f'{name}.pak', 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipdir(f'{name}/', zipf)
+        print('new file created..')
+    input('press enter to close..')
