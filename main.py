a6fa17a97ef244e3d1d7a9cc2e5006c1a2e4730b
21123-school <21123@haurakiplains.school.nz>
2025-06-06 17:31:21 +1200

    change lose numbers to vars

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import json
 import mapload
 from pygame.locals import QUIT
 
+size = 2
+aspect = (16,9)
+cellsize = 32
+halfcellsize = cellsize / 2
+
 # pygame set up
 pygame.init()
-screen = pygame.display.set_mode((18*32, 14*32))
+screen = pygame.display.set_mode(((aspect[0]*size)*cellsize, (aspect[1]*size)*cellsize))
 clock = pygame.time.Clock()
 fps = 120
 dt = 1
@@ -16,33 +21,37 @@ dt = 1
 
 class player:
 	def __init__(self, tilemap):
-		self.playeranimations = [pygame.transform.scale(pygame.image.load(mapload.loadpakitem(sys.argv[1],f'{arg}/textures/playerstand.png')), (32,32)), pygame.transform.scale(pygame.image.load(mapload.loadpakitem(sys.argv[1],f'{arg}/textures/playerwalk.png')), (32,32))]
+		self.playeranimations = [pygame.transform.scale(pygame.image.load(mapload.loadpakitem(sys.argv[1],f'{arg}/textures/playerstand.png')), (cellsize,cellsize)), pygame.transform.scale(pygame.image.load(mapload.loadpakitem(sys.argv[1],f'{arg}/textures/playerwalk.png')), (cellsize,cellsize))]
 		self.isonfloor = False
 		self.vely = 0
 		self.moving = True
 		self.animation = True
-		self.animationdelay = fps / 8
 		self.currentanimationdelay = 0
 		self.tilemap = tilemap
 		self.lasttime = 0
 		self.dir = 1
-		self.playerrect = pygame.Rect(( len(self.tilemap.currentlevel[0]) * 16 - 16, len(self.tilemap.currentlevel) * 16 - 16),(10,10))
+		self.maxfallspeed = 100
+		self.gravity = 40
+		self.jumphight = -120
+		self.speed = 38
+		self.playerrect = pygame.Rect(( len(self.tilemap.currentlevel[0]) * halfcellsize - halfcellsize, len(self.tilemap.currentlevel) * halfcellsize - halfcellsize),(10,10))
 	
 	def playermove(self):
+		self.animationdelay = int(clock.get_fps()) / 8
 		self.keys = pygame.key.get_pressed()
 		
-		self.BoolToInt = {True: 0, False: 19}
+		self.BoolToInt = {True: 0, False: self.speed}
 		self.movevector = pygame.math.Vector2( ( self.BoolToInt[self.keys[pygame.K_a]] + 0 - self.BoolToInt[self.keys[pygame.K_d]] ), ( self.BoolToInt[self.keys[pygame.K_w]] + 0 - self.BoolToInt[self.keys[pygame.K_s]] ) )
 
-		if not self.vely >= 35:
-			self.vely += 1
+		if not self.vely >= self.maxfallspeed:
+			self.vely += self.gravity * dt
 		if self.keys[pygame.K_w] and self.isonfloor:
-			self.vely = -55
+			self.vely += self.jumphight
 
 		self.isonfloor = False
 
 		self.tempx = self.tilemap.global_to_map(pygame.math.Vector2(self.playerrect.x + self.movevector.x, self.playerrect.y))
-		self.tempy = self.tilemap.global_to_map(pygame.math.Vector2(self.playerrect.x, self.playerrect.y + (self.vely * dt) + 16))
+		self.tempy = self.tilemap.global_to_map(pygame.math.Vector2(self.playerrect.x, self.playerrect.y + (self.vely * dt) + halfcellsize))
 
 		if self.tilemap.currentlevel[int(self.tempy.y)][int(self.tempy.x)] != 2:
 			self.playerrect.y += self.vely * dt
@@ -59,8 +68,11 @@ class player:
 		self.draw()
 
 	def draw(self):
+		if self.movevector.x != 0: 
+			self.dir = self.movevector.x 
+
 		if self.movevector.x != 0 or self.movevector.y != 0:
-			if self.movevector.x < 0:
+			if self.dir < 0:
 				screen.blit(pygame.transform.flip(self.playeranimations[{True:1,False:0}[self.animation]], True, False),(self.playerrect.x + camera.x, self.playerrect.y + camera.y))
 			else:
 				screen.blit(self.playeranimations[{True:1,False:0}[self.animation]],(self.playerrect.x + camera.x, self.playerrect.y + camera.y))
@@ -71,17 +83,20 @@ class player:
 			else:
 				self.currentanimationdelay += 1
 		else:
-			screen.blit(self.playeranimations[0], (self.playerrect.x + camera.x, self.playerrect.y + camera.y))
+			if self.dir < 0:
+				screen.blit(pygame.transform.flip(self.playeranimations[0], True, False),(self.playerrect.x + camera.x, self.playerrect.y + camera.y))
+			else:
+				screen.blit(self.playeranimations[0],(self.playerrect.x + camera.x, self.playerrect.y + camera.y))
 
 class tilemap:
 	def __init__(self,map):
 		self.currentlevel = map
 
 	def drawlevel(self):
-		tempcolor = [pygame.Color(100,255,0), pygame.Color(255,255,255), pygame.image.load(mapload.loadpakitem(sys.argv[1],f'{arg}/textures/missing.bmp'))]
+		tempcolor = [pygame.Color(100,255,0), pygame.Color(255,0,255), pygame.image.load(mapload.loadpakitem(sys.argv[1],f'{arg}/textures/missing.bmp'))]
 		for i in range(len(self.currentlevel)):
 			for j in range(len(self.currentlevel[i])):
-				self.tilerect = pygame.Rect( ((j*32) + camera.x, (i*32) + camera.y), (32,32) )
+				self.tilerect = pygame.Rect( ((j*cellsize) + camera.x, (i*cellsize) + camera.y), (cellsize,cellsize) )
 				try:
 					screen.blit(tempcolor[self.currentlevel[i][j]],self.tilerect)
 				except:
@@ -91,18 +106,13 @@ class tilemap:
 						pygame.draw.rect(screen, pygame.Color(255,255,255), self.tilerect) 
 
 	def global_to_map(self, vector):
-		x = int((vector.x + 16) // 32)
-		y = int((vector.y + 16) // 32)
+		x = int((vector.x + halfcellsize) // cellsize)
+		y = int((vector.y + halfcellsize) // cellsize)
 		return pygame.math.Vector2(x, y)
 
 def pygametext(txt):
 	sans = pygame.font.SysFont('Comic Sans MS', 20)
-	return sans.render(txt, False, (255, 255, 255))
-
-def loadjson(f):
-	with open(f, 'rb') as file:
-		o = json.load(file)
-	return o
+	return sans.render(txt, False, (155, 155, 155))
 
 arg = str(sys.argv[1]).split('.')[0]
 newmap, nextlevel = mapload.buildmap(mapload.loadpaktext(sys.argv[1], f"{arg}/maps/local.map"))
@@ -117,10 +127,10 @@ player = player(tilemap)
 processes = [tilemap.drawlevel, player.playermove]
 
 while True:
-	camera.x = 0 - player.playerrect.x + (18 * 16) - 16
-	camera.y = 0 - player.playerrect.y + (14 * 26) - 24
+	camera.x = 0 - player.playerrect.x + (aspect[0] * cellsize) - cellsize
+	camera.y = 0 - player.playerrect.y + (aspect[1] * (cellsize * 1.25)) - (cellsize * 1.25)
 
-	screen.fill((34, 34, 34))
+	screen.fill(pygame.Color(11,11,11))#to be removed
 
 	for process in processes:
 		process()
@@ -135,4 +145,4 @@ while True:
 	pygame.display.set_caption('FPS: ' + str(int(clock.get_fps())))
 
 	pygame.display.update()
-	dt = clock.tick(fps)/100
+	dt = clock.tick()/100
