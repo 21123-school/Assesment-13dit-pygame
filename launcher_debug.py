624f17ecbac2dd2330a1130d9f3f955cbeb52e75
21123-school <21123@haurakiplains.school.nz>
2025-10-13 21:35:29 +1300

    launcher update

--- launcher_debug.py
+++ launcher_debug.py
@@ -27,36 +27,74 @@ ttk.Label(frm, text="---------game launcher---------").grid(
 )
 
 # Dropdown label + combobox for GamePack (.pak files)
-ttk.Label(frm, text="Select GamePack:").grid(
-    column=0, row=1, sticky=W, padx=5, pady=5
+ttk.Label(frm, text="SELECT GAMEPACK:").grid(
+    column=0, row=1, sticky=W, padx=0, pady=15
 )
 selected_file_pak = StringVar()
 file_dropdown_pak = ttk.Combobox(frm, textvariable=selected_file_pak, values=files, state="readonly", width=30)
-file_dropdown_pak.grid(column=1, row=1, padx=5, pady=5)
+file_dropdown_pak.grid(column=1, row=1, padx=0, pady=15)
 if files:
     selected_file_pak.set(files[0])
 
 # Dropdown label + combobox for Sav (.sav files)
-ttk.Label(frm, text="Select Sav:").grid(
-    column=0, row=2, sticky=W, padx=5, pady=5
-)
-selected_file_sav = StringVar()
-file_dropdown_sav = ttk.Combobox(frm, textvariable=selected_file_sav, values=files2, state="readonly", width=30)
-file_dropdown_sav.grid(column=1, row=2, padx=5, pady=5)
+#ttk.Label(frm, text="Select Sav:").grid(
+#    column=0, row=2, sticky=W, padx=5, pady=5
+#)
+#selected_file_sav = StringVar()
+#file_dropdown_sav = ttk.Combobox(frm, textvariable=selected_file_sav, values=files2, state="readonly", width=30)
+#file_dropdown_sav.grid(column=1, row=2, padx=5, pady=5)
+
+ttk.Label(frm, text="HOST IP: ").grid(column=0, row=3, pady=(0, 15))
+file_dropdown_sav = ttk.Entry(frm)
+file_dropdown_sav.grid(column=1, row=3, pady=(0, 15))
+
+ttk.Label(frm, text="USERNAME: ").grid(column=0, row=4, pady=(0, 15))
+file_dropdown_nam = ttk.Entry(frm)
+file_dropdown_nam.grid(column=1, row=4, pady=(0, 15))
 
 # Start button
-def start_game():
+# Start button
+def start_game_S():
     pak = selected_file_pak.get()
-    sav = selected_file_sav.get()
-    subprocess.Popen([sys.executable, "main.py", pak, sav])
+    sav = file_dropdown_sav.get()
+    if not sav:
+        sav = "localhost"
+    nam = file_dropdown_nam.get()
+    if not nam:
+        nam = "   "
+    #for i in range(5):
+    os.system("server.py")
+    subprocess.Popen([sys.executable, "main.py", pak, sav, nam[:3]])
+    
+def start_game_M():
+    pak = selected_file_pak.get()
+    sav = file_dropdown_sav.get()
+    if not sav:
+        sav = "localhost"
+    nam = file_dropdown_nam.get()
+    if not nam:
+        nam = "   "
+    #for i in range(5):
+    subprocess.Popen([sys.executable, "main.py", pak, sav, nam[:3]])
 
-ttk.Button(frm, text="Start", command=start_game).grid(
-    column=0, row=3, columnspan=2, pady=(15, 5)
+def start_game_G():
+    os.system("server.py")
+
+ttk.Button(frm, text="SINGLE PLAYER", command=start_game_S).grid(
+    column=0, row=5, columnspan=2, pady=(0, 15)
+)
+
+ttk.Button(frm, text="JOIN MULTIPLAYER", command=start_game_M).grid(
+    column=0, row=6, columnspan=3, pady=(0, 15)
+)
+
+ttk.Button(frm, text="START SERVER", command=start_game_G).grid(
+    column=0, row=7, columnspan=2, pady=(0, 15)
 )
 
 # Quit button
-ttk.Button(frm, text="Quit", command=root.destroy).grid(
-    column=0, row=4, columnspan=2, pady=(5, 0)
+ttk.Button(frm, text="QUIT", command=root.destroy).grid(
+    column=0, row=8, columnspan=4, pady=(0, 15)
 )
 
 # Center window
